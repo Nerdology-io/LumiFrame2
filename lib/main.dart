688656c62e03d4d28965d432cd88fd3c648bd96c
UndercoverNerd5 <218186149+Nerdology-io@users.noreply.github.com>
2025-07-15 16:47:18 -0700

    Initial Push with Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'bindings/initial_bindings.dart';
-// import 'routes/app_routes.dart';
-// import 'firebase_options.dart';
+import 'routes/app_routes.dart';
+import 'firebase_options.dart';
 import 'theme/app_themes.dart';
 import 'controllers/theme_controller.dart';
 import 'screens/onboarding/onboarding_start.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Get.put(ThemeController());
   runApp(const LumiFrameApp());
 }
